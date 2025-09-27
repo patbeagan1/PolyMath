0a10b76f076ee681753b837f4f6e852549bed9d2
pbeagan <pbeagan1@gmail.com>
2025-09-27 00:28:27 -0400

    Moving common units out

---
+++ unit-common/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    kotlin("multiplatform") version "1.9.20"
+}
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "17"
+        }
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+    js(IR) {
+        browser {
+            commonWebpackConfig {
+                cssSupport {
+                    enabled.set(true)
+                }
+            }
+        }
+        nodejs()
+    }
+    linuxX64()
+
+    sourceSets {
+        commonMain.dependencies {
+            api(project(":unit-base"))
+            implementation(kotlin("test"))
+        }
+    }
+}
